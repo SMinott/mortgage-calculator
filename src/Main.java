ca03aa7841fcf91d5d45e6940ea2a7c11aedb754
Susan Minott <susan_minott@yahoo.com>
2023-04-21 13:25:15 -0400

    Updated Variables_Added CONSTANTS

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,30 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
-//    Create a mortgage calculator to calculate the monthly mortgage payment.
-//    Input the Principal, Annual Int. Rate, & Period.
-//    M = P * [r(1+r)^n] / [(1+r)^n -1]
-//    M = Mortgage must be expressed as a dollar amount with sign & decimal (Math method)
-//    P = total amount owed.
-//    n = number of payments P * 12
-//    r = Annual Int. Rate must be divided by 12 and 100 to get the monthly rate.
+    static double mortgageNum(int principal, float annualIntRate, int period ){
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+        double mthlyIntRate = annualIntRate / MONTHS_IN_YEAR / PERCENT;
+        int numberOfPayments = period * MONTHS_IN_YEAR;
 
-    static double mortgageNum(int p, float r, int n ){
-        double power = Math.pow((1 + r), n);
-        double mthlyMortgage  = p * (r * power) / (power - 1);
-        return mthlyMortgage;
+        //      Equation: M = P * (r(1+r)^n) / (1+r)^n -1
+        double powerMethod = Math.pow((1 + mthlyIntRate), numberOfPayments);
+        double mthlyMortgageCalculation  = principal * (mthlyIntRate * powerMethod) / (powerMethod - 1);
+
+        return mthlyMortgageCalculation;
     }
     public static void main(String[] args) {
 
         Scanner num = new Scanner(System.in);
 
-        System.out.print("What is your Principal? ");
+        System.out.print("What is your Principal amount? ");
         int p = num.nextInt();
 
         System.out.print("What is your Annual Interest Rate? ");
-        float r = num.nextFloat() /12 /100;
+        float r = num.nextFloat();
 
-        System.out.print("What is your mortgage Period? ");
-        int n = num.nextInt() * 12;
+        System.out.print("What is your mortgage Period(years)? ");
+        int n = num.nextInt();
 
 //        So the monthly mortgage amount me be formatted as currency.
         String mortgage = NumberFormat.getCurrencyInstance().format(mortgageNum(p, r, n));
